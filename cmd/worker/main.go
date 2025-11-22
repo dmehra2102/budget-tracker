c5c25b7c986109fee30cdef3d86f3c9146147f5b
Deepanshu Mehra <deepanshumehra2102@gmail.com>
2025-11-22 11:49:51 +0530

    feat: added worker entrypoint file (main.go)

---
+++ cmd/worker/main.go
@@ -0,0 +1,53 @@
+package main
+
+import (
+	"log"
+	"os"
+	"os/signal"
+	"syscall"
+
+	"github.com/dmehra2102/budget-tracker/internal/config"
+	"github.com/dmehra2102/budget-tracker/internal/database"
+	"github.com/dmehra2102/budget-tracker/internal/repository"
+	"github.com/dmehra2102/budget-tracker/internal/service"
+	"github.com/dmehra2102/budget-tracker/internal/worker"
+)
+
+func main() {
+	cfg, err := config.Load()
+	if err != nil {
+		log.Fatalf("Failed to load config: %v", err)
+	}
+
+	db,err := database.NewMongoDB(cfg)
+	if err != nil {
+        log.Fatalf("Failed to connect to MongoDB: %v", err)
+    }
+    defer db.Close()
+
+    // Initialize repositories
+    userRepo := repository.NewUserRepository(db.DB())
+    budgetRepo := repository.NewBudgetRepository(db.DB())
+    alertRepo := repository.NewAlertRepository(db.DB())
+
+	// Initialize Services
+	emailService := service.NewEmailService(cfg)
+	alertService := service.NewAlertService(alertRepo, budgetRepo, userRepo, emailService)
+
+	cronWorker := worker.NewCronWorker(cfg, db.DB(), alertService, budgetRepo)
+
+	if err := cronWorker.Start(); err != nil {
+		log.Fatalf("Failed to start worker: %v", err)
+	}
+
+	log.Println("Worker Started Successfully")
+
+	// Wait for interrupt signal
+    quit := make(chan os.Signal, 1)
+    signal.Notify(quit, syscall.SIGINT, syscall.SIGTERM)
+    <-quit
+
+    log.Println("Shutting down worker...")
+    cronWorker.Stop()
+    log.Println("Worker exited")
+}
